1ffba049e0d3a8f0560f9a36283201a8de2f438d
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-23 22:42:19 -0400

    commit tree d13bf0ffa67d539eb08f93ada24947ed47bdf817

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", "victory")
+const textNode = document.createTextNode("Danny is the champion");
+newHeader.appendChild(textNode);
